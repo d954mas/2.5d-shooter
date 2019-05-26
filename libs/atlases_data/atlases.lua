88478183e63f37604adc0c72085fa6d15bf9d0c9
d954mas <d954mas@mail.ru>
2019-05-26 18:32:58 +0300

    add render_cam

--- libs/atlases_data/atlases.lua
+++ libs/atlases_data/atlases.lua
@@ -3,7 +3,6 @@ local M = {}
 
 M.GAME = require "libs.atlases_data.game"
 M.GUI = require "libs.atlases_data.gui"
-M.ICONS = require "libs.atlases_data.icons"
 
 
 return COMMON.read_only_recursive(M)
