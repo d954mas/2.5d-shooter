ed52398fb6f474f29e381c4fc4ab90c49e4d0fe6
d954mas <d954mas@mail.ru>
2019-07-09 02:53:08 +0300

    fixed scene manager for release

--- libs/sm/loader.lua
+++ libs/sm/loader.lua
@@ -11,37 +11,37 @@ M.scene_loaded = {} -- all loading proxy
 ---@param scene Scene
 ---@return Observable
 function M.load(scene)
-    assert(not M.scene_load[tostring(scene._url)], " scene is loading now:" .. scene._name)
+    assert(not M.scene_load[tostring(scene._url.path)], " scene is loading now:" .. scene._name)
     local s = RX.Subject()
     if M.is_loaded(scene) then
         COMMON.w("scene:" .. scene._name .. " already loaded")
         s:onCompleted()
         return
     end
-    M.scene_load[tostring(scene._url)] = s
+    M.scene_load[tostring(scene._url.path)] = s
     msg.post("main:/scene_loader", COMMON.HASHES.MSG_SM_LOAD, { url = scene._url})
     return s
 end
 
 function M.is_loaded(scene)
-    return M.scene_loaded[tostring(scene._url)]
+    return M.scene_loaded[tostring(scene._url.path)]
 end
 
 function M.load_done(url)
-    local subject = M.scene_load[tostring(url)]
+    local subject = M.scene_load[tostring(url.path)]
     if subject then
-        M.scene_load[tostring(url)] = nil
-        M.scene_loaded[tostring(url)] = true
+        M.scene_load[tostring(url.path)] = nil
+        M.scene_loaded[tostring(url.path)] = true
         subject:onCompleted()
     else
-        COMMON.w("scene:" .. tostring(url) .. " not wait for loading",TAG)
+        COMMON.w("scene:" .. tostring(url.path) .. " not wait for loading",TAG)
     end
 end
 
 function M.unload(scene)
     msg.post(scene._url, COMMON.HASHES.MSG_UNLOAD)
-    M.scene_load[tostring(scene._url)] = false
-    M.scene_loaded[tostring(scene._url)] = false
+    M.scene_load[tostring(scene._url.path)] = false
+    M.scene_loaded[tostring(scene._url.path)] = false
 end
 
 return M
