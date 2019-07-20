5be1d94a60392b883ce825c761ce33ba4e04bd9b
d954mas <d954mas@mail.ru>
2019-07-20 23:19:47 +0300

    fixed culling objects bad color

--- world/ecs/systems/update_object_color_system.lua
+++ world/ecs/systems/update_object_color_system.lua
@@ -8,12 +8,14 @@ System.filter = ECS.requireAll("dynamic_color","url_sprite")
 --it break batching.Maybe store center position in coordinates.Storing in coordinates break physics.
 --set color only for visible.Invisible use same cell.Reduces draw calls
 --change sprite constant only on changes.Reduces messages
---
+
+ local EMPTY_VECTOR = vmath.vector4(0,0,0,0)
+
 ---@param e Entity
 function System:process(e, dt)
 	local cell_x,cell_y = math.ceil(e.position.x), math.ceil(e.position.y)
 	local visible = native_raycasting.cells_get_by_coords(cell_x,cell_y):get_visibility()
-	local color_cell =  visible and vmath.vector4(cell_x-0.5,cell_y-0.5,0,0) or vmath.vector4(0,0,0,0)
+	local color_cell =  visible and vmath.vector4(cell_x-0.5,cell_y-0.5,0,0) or EMPTY_VECTOR
 	if e.dynamic_color_cell ~= color_cell then
 		e.dynamic_color_cell = color_cell
 		sprite.set_constant(e.url_sprite,HASH_OBJECT_POSITION,e.dynamic_color_cell)
