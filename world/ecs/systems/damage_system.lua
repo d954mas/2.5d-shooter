dddcfea8d31f97a8867f05f1d2f0b40d436ae1f9
d954mas <d954mas@mail.ru>
2019-07-20 23:08:35 +0300

    fixed ignore damage after receive damage

--- world/ecs/systems/damage_system.lua
+++ world/ecs/systems/damage_system.lua
@@ -8,7 +8,7 @@ System.filter = ECS.requireAll("damage_info")
 ---@param e Entity
 function System:process(e, dt)
 	local info = e.damage_info
-	if info.target_e.hp and info.target_e.hp > 0 then
+	if info.target_e.hp and info.target_e.hp > 0 and not info.target_e.ignore_damage then
 		info.target_e.hp = math.max(0,info.target_e.hp - info.weapon_prototype.damage)
 		if info.target_e.hp == 0 and not info.target_e.player then
 			SOUNDS:play_sound(SOUNDS.sounds.game.monster_blob_die)
