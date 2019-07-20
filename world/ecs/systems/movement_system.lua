0306043525c80801ba8d4b79ccd22a9078913403
d954mas <d954mas@mail.ru>
2019-07-20 22:03:28 +0300

    add movement system comments

--- world/ecs/systems/movement_system.lua
+++ world/ecs/systems/movement_system.lua
@@ -5,16 +5,13 @@ System.filter = ECS.requireAll("position","movement_direction","movement_velocit
 
 ---@param e Entity
 function System:process(e, dt)
+	--normalize keyboard input
 	if e.movement_direction.x ~= 0 and e.movement_direction.y~=0 then
 		e.movement_direction = vmath.normalize(e.movement_direction)
 	end
-
-	--first
+	--first type. Inertia when change camera direction
 	if not toggle_1 and  e.angle then e.movement_direction = vmath.rotate(vmath.quat_rotation_z(e.angle.x),e.movement_direction) end
-
-
 	local target = e.movement_direction * (e.movement_max_speed or 3)
-
 	local accel
 	--Take the dot product of target to see if the player is moving according to target
 	if vmath.dot(target,e.movement_velocity) > 0 then
@@ -24,9 +21,8 @@ function System:process(e, dt)
 	end
 
 	e.movement_velocity = vmath.lerp(accel * dt, e.movement_velocity,target)
-
 	local vel = vmath.vector3(e.movement_velocity)
-	--second
+	--second type Inertia only when change direction(keyboard keys)
 	if toggle_1 and e.angle then vel = vmath.rotate(vmath.quat_rotation_z(e.angle.x),vel) end
 
 	e.position.x = e.position.x + vel.x*dt
