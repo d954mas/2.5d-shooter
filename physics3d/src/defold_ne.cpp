92acec378ea396b4972c76e3f2d0e58accd504de
d954mas <d954mas@gmail.com>
2020-03-20 21:59:57 +0300

    physics compiled

---
+++ physics3d/src/defold_ne.cpp
@@ -0,0 +1,46 @@
+// myextension.cpp
+// Extension lib defines
+#define EXTENSION_NAME physics3d
+#define LIB_NAME "physics3d"
+#define MODULE_NAME "physics3d"
+// include the Defold SDK
+#include <dmsdk/sdk.h>
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] =
+{	
+	{0, 0}
+};
+
+static void LuaInit(lua_State* L)
+{
+	int top = lua_gettop(L);
+	// Register lua names
+	luaL_register(L, MODULE_NAME, Module_methods);
+	lua_pop(L, 1);
+	assert(top == lua_gettop(L));
+}
+
+static dmExtension::Result AppInitializeMyExtension(dmExtension::AppParams* params)
+{
+	return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result InitializeMyExtension(dmExtension::Params* params)
+{
+	// Init Lua
+	LuaInit(params->m_L);
+	printf("Registered %s Extension\n", MODULE_NAME);
+	return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result AppFinalizeMyExtension(dmExtension::AppParams* params)
+{
+	return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeMyExtension(dmExtension::Params* params)
+{
+	return dmExtension::RESULT_OK;
+}
+
+DM_DECLARE_EXTENSION(EXTENSION_NAME, LIB_NAME, AppInitializeMyExtension, AppFinalizeMyExtension, InitializeMyExtension, 0, 0, FinalizeMyExtension)
