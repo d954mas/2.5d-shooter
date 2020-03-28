831968ae989b591aa72c474b8194ddc00aad7da4
d954mas <d954mas@gmail.com>
2020-03-28 12:08:08 +0300

    fixed url go

--- model/battle/ecs/systems/update_go_system.lua
+++ model/battle/ecs/systems/update_go_system.lua
@@ -1,21 +1,22 @@
 local ECS = require 'libs.ecs'
 ---@class UpdateGoSystem:ECSSystem
 local System = ECS.processingSystem()
-System.filter = ECS.requireAll("url_go")
+System.filter = ECS.requireAll("level_object_go")
 System.name = "UpdateGoSystem"
 
 ---@param e Entity
 function System:process(e, dt)
-	if (not e.url_go) then return end
+	local url_go = e.level_object_go and e.level_object_go.root
+	if (not url_go) then return end
 	if e.position then
 		local current_pos = vmath.vector3(e.position.x, e.position.z, -e.position.y)
-		go.set_position(current_pos, e.url_go)
+		go.set_position(current_pos,url_go)
 	end
 
 	if e.rotation_look_at_player_quaternion then
-		go.set_rotation(e.rotation_look_at_player_quaternion, e.url_go)
+		go.set_rotation(e.rotation_look_at_player_quaternion, url_go)
 	elseif e.angle then
-		go.set_rotation(vmath.quat_rotation_y(e.angle.x), e.url_go)
+		go.set_rotation(vmath.quat_rotation_y(e.angle.x), url_go)
 	end
 
 end
