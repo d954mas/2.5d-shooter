4b84504ed574dfc94e5a890539717eca014c2780
d954mas <d954mas@gmail.com>
2020-04-12 22:32:31 +0300

    reduce velocity when collide with wall

--- model/battle/ecs/systems/physics_collision_wall_system.lua
+++ model/battle/ecs/systems/physics_collision_wall_system.lua
@@ -30,17 +30,30 @@ function System:handle_collision(info)
 
 
 end
-
+---@param e Entity
 function System:handle_geometry(e, normal, distance)
 	assert(e);
 	assert(normal);
 	assert(distance)
 	local correction = assert(e.physics_obstacles_correction)
-	if (vmath.length(normal * distance) <= 0) then return end
+	local nd = normal * distance
+	if (vmath.length(nd) <= 0) then return end
 	if distance > 0 then
-		local proj = vmath.project(correction, normal * distance)
+		local proj = vmath.project(correction, nd)
 		if proj < 1 then
+
 			local comp = (distance - distance * proj) * normal
+		--	pprint(e.movement.velocity)
+			--little bit reduce velocity when collide with wall.Only for one collision per frame
+			--empirical formula
+			if(e.physics_obstacles_correction.x ==0 and e.physics_obstacles_correction.y == 0)then
+				local power_x = math.min(math.abs(nd.x)*55,1)
+				local power_y = math.min(math.abs(nd.y)*55,1)
+				e.movement.velocity.x = e.movement.velocity.x - e.movement.velocity.x*0.1*power_x
+				e.movement.velocity.y = e.movement.velocity.y - e.movement.velocity.y*0.1*power_y
+			end
+
+
 			e.position.x = e.position.x + comp.x
 			e.position.y = e.position.y - comp.y
 			e.physics_obstacles_correction = correction + comp
