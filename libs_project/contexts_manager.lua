046a91983567afa8bc3c7bd832555f800308d459
d954mas <d954mas@gmail.com>
2020-05-02 23:13:44 +0300

    show title when look at door

--- libs_project/contexts_manager.lua
+++ libs_project/contexts_manager.lua
@@ -7,6 +7,7 @@ local Manager = CLASS.class("ContextManagerProject", ContextManager)
 Manager.NAMES = {
 	MAIN = "MAIN",
 	GAME = "GAME",
+	GAME_GUI = "GAME_GUI",
 	RENDER = "RENDER",
 	LIGHT_MAP_SCRIPT = "LIGHT_MAP_SCRIPT",
 	DYNAMIC_LIGHT = "DYNAMIC_LIGHT",
@@ -18,6 +19,14 @@ Manager.NAMES = {
 ---@return ContextStackWrapperMain
 function Manager:set_context_top_main()
 	return self:set_context_top_by_name(self.NAMES.MAIN)
+end
+
+---@class ContextStackWrapperGameGui:ContextStackWrapper
+-----@field data GameGuiScript
+
+---@return ContextStackWrapperGameGui
+function Manager:set_context_game_gui()
+	return self:set_context_top_by_name(self.NAMES.GAME_GUI)
 end
 
 return Manager()
--- model/battle/ecs/systems/action_open_door_system.lua
+++ model/battle/ecs/systems/action_open_door_system.lua
@@ -1,3 +1,4 @@
+local COMMON = require "libs.common"
 local ECS = require 'libs.ecs'
 
 ---@class ActionOpenDoorActionSystem:ECSSystem
@@ -9,12 +10,15 @@ function System:update(dt)
 	local game = self.world.game
 	local result = game:raycast(game.player, 1, physics3d.GROUPS.OBSTACLE)
 	local object = result[1]
+	local ctx = COMMON.CONTEXT:set_context_game_gui()
+	ctx.data:action_info_set_visible(false)
 	if(object) then
 		local e = object.body:get_user_data()
 		if(e.door) then
-			pprint("door")
+			ctx.data:action_info_set_visible(true)
 		end
 	end
+	ctx:remove()
 end
 
 return System
